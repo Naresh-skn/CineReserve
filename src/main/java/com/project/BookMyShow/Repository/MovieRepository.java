d191ba8b3a9f95aa9671d0e2fdbdffae241cf3f1
Naresh-skn <nareshskn26@gmail.com>
2024-10-23 06:59:42 +0530

    Repo

---
+++ src/main/java/com/project/BookMyShow/Repository/MovieRepository.java
@@ -0,0 +1,13 @@
+package com.project.BookMyShow.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.BookMyShow.Entity.Movie;
+
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+	Movie findByTitle(String title);
+	
+
+}
